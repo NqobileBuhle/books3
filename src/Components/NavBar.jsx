221891db21a4877165c4f138b89d5f1877546fb9
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2024-10-18 11:47:56 +0200

    logo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='bg-secondary w-full left-0 z-10 px-36 py-5'>
+    <div className='bg-secondary w-full left-0 z-10 px-36 py-5 '>
       <div className=' flex justify-between items-center'>
-        <a href=''>eBOOKS </a>
+        <a className='font-bold text-2xl' href=''>eBOOKS </a>
 
         <input 
         className='g-white text-gray-400 px-2' placeholder='Search for books'
